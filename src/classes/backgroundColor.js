e63fb2bdc0e1e5dd57b42c2932788c106249b969
mthdht <mthdht@gmail.com>
2024-03-21 10:28:41 +0100

    create background color classes for components

---
+++ src/classes/backgroundColor.js
@@ -0,0 +1,44 @@
+export const backgroundColor = {
+    gray: {
+      fill: 'bg-slate-500 text-white',
+      light: 'bg-slate-100 text-slate-600',
+      outlined: 'text-slate-500 border-2 border-slate-500',
+      text: 'text-slate-600',
+      none: '',
+    },
+    red: {
+      fill: 'bg-red-500 text-white',
+      light: 'bg-red-100 text-red-600',
+      outlined: 'text-red-500 border-2 border-red-500',
+      text: 'text-red-600',
+      none: '',
+    },
+    orange: {
+      fill: 'bg-orange-500 text-white',
+      light: 'bg-orange-100 text-orange-600',
+      outlined: 'text-orange-500 border-2 border-orange-500',
+      text: 'text-orange-600',
+      none: '',
+    },
+    yellow: {
+      fill: 'bg-yellow-500 text-white',
+      light: 'bg-yellow-100 text-yellow-600',
+      outlined: 'text-yellow-500 border-2 border-yellow-500',
+      text: 'text-yellow-600',
+      none: '',
+    },
+    green: {
+      fill: 'bg-emerald-500 text-white',
+      light: 'bg-emerald-100 text-emerald-600',
+      outlined: 'text-emerald-500 border-2 border-emerald-500',
+      text: 'text-emerald-600',
+      none: '',
+    },
+    blue: {
+      fill: 'bg-sky-500 text-white',
+      light: 'bg-sky-100 text-sky-600',
+      outlined: 'text-sky-500 border-2 border-sky-500',
+      text: 'text-sky-600',
+      none: '',
+    },
+  };
