b134b9869ee62978638829b411b03e5b97c36479
mthdht <mthdht@gmail.com>
2024-11-07 08:35:17 +0100

    add tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: path.resolve(__dirname, 'src/library.js'),
+      entry: resolve(__dirname, 'src/library.js'),
       name: 'Lantern',
       // the proper extensions will be added
       fileName: 'lantern',
