95aed688c79287d54d107f39747048d72aa34be6
mthdht <mthdht@gmail.com>
2025-07-03 09:48:31 +0200

    made searchList component

---
+++ src/components/searchList/SearchList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div 
+        class="l-search-list" 
+        :class="pharosClass('Collapsible', props.class)"
+        v-bind="visibleAttrs"
+    >
+        <slot></slot>    
+    </div>
+</template>
+
+<script setup lang="ts">
+import { provide, type HTMLAttributes } from 'vue'
+import { usePharosComponent } from '../../composables/usePharosComponent';
+
+const props = defineProps<{
+    class?: HTMLAttributes['class'],
+    [key: string]: unknown
+}>()
+
+const emits = defineEmits()
+
+defineOptions({
+    inheritAttrs: false
+})
+
+const { visibleAttrs, pharosClass } = usePharosComponent()
+const model = defineModel()
+
+provide('searchList', {
+    model,
+})
+</script>
+
+<style scoped>
+
+</style>
