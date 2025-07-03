1a476771c785388997865d085bae859fb3751a10
mthdht <mthdht@gmail.com>
2025-07-03 10:01:18 +0200

    made searchList input component

---
+++ src/components/searchList/SearchListInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <input 
+        v-model="searchList?.model.value"
+        type="text" 
+        class="l-search-list-input" 
+        :class="pharosClass('Collapsible', props.class)"
+        v-bind="visibleAttrs"
+    >
+</template>
+
+<script setup lang="ts">
+import { inject, type HTMLAttributes, type Ref } from 'vue'
+import { usePharosComponent } from '../../composables/usePharosComponent'
+
+const props = defineProps<{
+    class?: HTMLAttributes['class'],
+    [key: string]: unknown
+}>()
+
+defineOptions({
+    inheritAttrs: false
+})
+
+const { visibleAttrs, pharosClass } = usePharosComponent()
+
+const searchList = inject<{
+    model: Ref
+}>('searchList')
+
+</script>
